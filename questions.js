d1a4edab5a7943b3b8fe0ebac6fc252fe30906a8
jmay899 <79422971+jmay899@users.noreply.github.com>
2021-10-17 19:24:16 -0400

    Added multiple sets of questions

---
+++ questions.js
@@ -0,0 +1,61 @@
+//Geography/History
+let geographyHistoryQuestions = [
+    {
+        question: "What is the capital of the United States?",
+        answer: "Washington D.C.",
+        options: ["New York City","Washington D.C.","Los Angeles","Chicago"]
+    },
+
+    {
+        question: "Which country has the largest population?",
+        answer: "China",
+        options: ["Russia","United States","India","China"]
+    },
+
+    {
+        question: "Which of the following is " + "not ".bold() + " a real ocean?",
+        answer: "Northern Ocean",
+        options: ["Northern Ocean","Atlantic Ocean","Indian Ocean","Pacific Ocean"]
+    },
+
+    {
+        question: "Which country is called the 'Land of the Rising Sun'?",
+        answer: "Japan",
+        options: ["China","Japan","South Korea","Taiwan"]
+    },
+
+    {
+        question: "Who was the first president of the United States?",
+        answer: "George Washington",
+        options: ["Abraham Lincoln","John F. Kennedy","George Washington","Thomas Jefferson"]
+    }
+]
+
+//Math
+let mathQuestions = [
+    {
+        question: "What is 6+7?",
+        answer: "13",
+        options: ["11","13","15","42"]
+    },
+    {
+        question: "What is the square root of 49?",
+        answer: "7",
+        options: ["5","6","7","8"]
+    },
+    {
+        question: "A rectangle has sides of 13 inches and 6 inches. What is the perimeter?",
+        answer: "38 inches",
+        options: ["38 inches","51 inches","65 inches","78 inches"]
+    },
+    {
+        question: "What is the formula for area of a circle?",
+        answer: "πr²",
+        options: ["(4/3)πr³","2πr","πr²","2πr²"]
+    },
+    {
+        question: "Solve for x: 9x+8=71",
+        answer: "7",
+        options: ["6","7","8","9"]
+    }
+]
